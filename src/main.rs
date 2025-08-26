c2969756af692933815a82ee77d9fca382961bfc
Robin <1187338299@qq.com>
2025-08-27 00:38:47 +0800

    feat: init clap crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{  Parser, Subcommand };
+
+
+#[derive(Parser)]
+#[command(name = "cli", version = "0.0.1")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add { name: String },
+    Delete { id: u32 },
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    match args.command {
+        Commands::Add { name } => println!("Added: {}", name),
+        Commands::Delete { id } => println!("Deleted: {}", id),
+    }
 }
