03438e2bc3c96362db04db202ef083e5ad214a90
jichuanche <jichuanche@pplabs.org>
2025-08-27 17:44:09 +0800

    feat: add open file features

--- src/cli/command.rs
+++ src/cli/command.rs
@@ -1,5 +1,5 @@
 use crate::cli::args::{Cli, Commands};
-use crate::core::search_files;
+use crate::core::file_opener::file_open;
 
 
 pub fn handle_cli(args: Cli) {
@@ -12,7 +12,6 @@ pub fn handle_cli(args: Cli) {
 
 fn handle_open_command(search_term: String) {
     println!("Added: {}", search_term);
-    let root_path = "D:/Weixin";
-    search_files(root_path, search_term.as_str());
-    opener::open("D:/Weixin/Weixin.exe").unwrap();
+    file_open(search_term.as_str());
+    // opener::open("D:/Weixin/Weixin.exe").unwrap();
 }
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,4 +1,3 @@
 pub mod file_opener;
 
 // 重新导出常用函数，方便外部使用
-pub use file_opener::search_files;
